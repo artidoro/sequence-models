59981f007e22730b5da9b6fd776774d0b1a7e6cf
yujiex <shenqing1225@gmail.com>
2019-11-29 12:54:47 -0500

    Added some notes

--- reber_grammar_data_generation.py
+++ reber_grammar_data_generation.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import random
 
+# generating embedded Reber Grammar sequence
+# detailed on page 1751 of the following paper
+# https://www.mitpressjournals.org/doi/pdfplus/10.1162/neco.1997.9.8.1735
 def gen_reber_seq():
     df0 = pd.DataFrame({"emission":["T", "P"], "next_state":[1, 2]})
     df1 = pd.DataFrame({"emission":["S", "X"], "next_state":[1, 3]})
@@ -14,12 +17,10 @@ def gen_reber_seq():
     reber_seq = [head_tail]
     states = []
     current_state = 0
-    print()
     while(current_state != 5):
         states.append(current_state)
         row = dfs[current_state].sample(n=1, replace=False)
         observed = row.iloc[0]["emission"]
-        # print("{},{}".format(current_state, observed))
         current_state = row.iloc[0]["next_state"]
         reber_seq.append(observed)
     reber_seq.append(head_tail)
