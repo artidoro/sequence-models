2bd6da2d4c7b41cf3ec54fbfcdd28e314beec20c
Ini <ini@thousandeyes.csail.mit.edu>
2019-11-30 01:37:21 -0500

    Remove extraneous  references in scheduler function

--- models/sequence_model.py
+++ models/sequence_model.py
@@ -17,23 +17,28 @@ def get_optimizer(model, lr=c.DEFAULT_LEARNING_RATE, momentum=0.0):
         return optim.Adagrad(model.parameters(), lr=lr)
 
 
-def get_scheduler(self, optimizer, scheduler):
+def get_scheduler(self, optimizer, scheduler, eta_min=0, 
+    max_step=100000, warmup_step=0, decay_rate=0.5, 
+    patience=0, min_lr=0.0):
+    
     if scheduler == 'cosine':
-        eta_min = self.eta_min if self.eta_min is not None else 0
-        return optim.lr_scheduler.CosineAnnealingLR(self.optimizer, self.max_step, eta_min=eta_min)
+        return optim.lr_scheduler.CosineAnnealingLR(optimizer, max_step, eta_min=eta_min)
     elif scheduler == 'inv_sqrt':
         def lr_lambda(step):
             # return a multiplier instead of a learning rate
-            if step == 0 and self.warmup_step == 0:
+            if step == 0 and warmup_step == 0:
                 return 1.
             else:
-                return 1. / (step ** 0.5) if step > self.warmup_step \
-                       else step / (self.warmup_step ** 1.5)
-        return optim.lr_scheduler.LambdaLR(self.optimizer, lr_lambda=lr_lambda)
+                return 1. / (step ** 0.5) if step > warmup_step \
+                       else step / (warmup_step ** 1.5)
+        return optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
     elif scheduler == 'dev_perf':
-        return optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=self.decay_rate, patience=self.patience, min_lr=self.lr_min)
+        return optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, factor=self.decay_rate, patience=self.patience, min_lr=self.lr_min)
     elif scheduler == 'constant':
         pass
+
+
 
 class SequenceModel(ABC):
     """
@@ -67,3 +72,4 @@ class SequenceModel(ABC):
 
     def get_model(self):
         return self.model
+
