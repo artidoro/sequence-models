0c26adc9ea777bb1cc76e892f549068fe60c4305
William Guss <carlostnb@gmail.com>
2019-11-30 00:01:31 -0500

    Specifier is complete.

--- config.py
+++ config.py
@@ -1,5 +1,18 @@
 """Specifies non variable parameters for the experimentation framework.
 """
+from collections import OrderedDict
 
 EXPERIMENT_RUNNER_SHOULD_OVERWRITE = True
 CVISIBLE = 'CUDA_VISIBLE_DEVICES'
+
+HYPERPARAMETERS = OrderedDict({
+    'algorithms': [
+        'cnn',
+        'lstm',
+        'lstm_attention',
+    ],
+    'depth': [2,4,8,16],
+    'width': [16,32,64,128,512,1024],
+    'vocab': [5000],
+    'sequence_dependence': [2,4,8,16,32]
+})
